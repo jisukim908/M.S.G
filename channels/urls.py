f9d44eb4a79ab60ae103d217b14112be5ce0ff4a
chanhyoYun <chloeee2@naver.com>
2023-05-09 21:03:46 +0900

    회원가입, 로그인, 수정 완료

--- channels/urls.py
+++ channels/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from channels import views
 
 urlpatterns = [
-    path('/api/channel/{user_id}/', views.ChannelsView(), name="channel_view"),
-    path('/api/channel/admin/', views.ChannelAdminView(), name="channel_admin_view"),
-    path('/api/channel/admin/{feed_id}/', views.ChannelAdminView(), name="update_view"),
+
 ]
---
+++ users/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from users.models import User, Tag
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name', 'id']
+    
+    def __str__(self):
+        return self.name
+
+class UserViewSerializer(serializers.ModelSerializer):
+    tags = serializers.StringRelatedField(many=True)
+    followings = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'username', 'profile_image', 'bio', 'joined_at', 'tags', 'followings']
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.profile_image = validated_data.get('profile_image', instance.profile_image)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.tags = validated_data.get('tags', instance.tags)
+        instance.followings = validated_data.get('followings', instance.followings)
+        instance.save()
+        return instance
+    
+# 회원가입
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'username', 'profile_image', 'bio', 'joined_at', 'tags', 'followings']
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.profile_image = validated_data.get('profile_image', instance.profile_image)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.tags = validated_data.get('tags', instance.tags)
+        instance.followings = validated_data.get('followings', instance.followings)
+        instance.save()
+        return instance
+
+class MyTokenObtainPairSerializer(TokenObtainPairView):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
+    
