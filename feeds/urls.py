c0972b1da6365b737fd64d61b94ba633664aa117
dana <bear9473@gmail.com>
2023-05-10 00:29:27 +0900

    feed관련 url추가

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from feeds import views
+
+urlpatterns = [
+    path('api/feeds/', views.FeedListView.as_view(), name="feedlist"),
+    path('api/feeds/<int:feed_id>/', views.FeedDetailView.as_view(), name="feed_detail"),
+    path('api/feeds/<int:feed_id>/likes/', views.LikeView.as_view(), name="likes_feed"),
+]
