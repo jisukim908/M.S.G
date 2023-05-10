9c5966efad8cf4b8a7200e9db4d1a18f6f832028
dana <bear9473@gmail.com>
2023-05-10 12:26:00 +0900

    bugfix: 충돌오류 해결

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,31 +1,7 @@
 from django.urls import path
-from .views import (
-    FeedListView,
-    FeedDetailView,
-    FeedCreateView,
-    CommentsView,
-    CommentsLikeView,
-    CommentsDislikeView,
-)
+from feeds import views 
 
 urlpatterns = [
-    path("feedlist/", FeedListView.as_view(), name="feed_list"),
-    path("feed/<int:post_id>/", FeedDetailView.as_view(), name="feed_detail"),
-    path("feed/create/", FeedCreateView.as_view(), name="feed_create"),
-    path("feed/<int:post_id>/update/", FeedCreateView.as_view(), name="feed_update"),
-    path("feed/<int:post_id>/delete/", FeedCreateView.as_view(), name="feed_delete"),
-    path("comments/", CommentsView.as_view(), name="comments"),
-    path("comments/<int:comment_id>/", CommentsView.as_view(), name="comment_detail"),
-    path(
-        "comments/<int:comment_id>/like/",
-        CommentsLikeView.as_view(),
-        name="comment_like",
-    ),
-    path(
-        "comments/<int:comment_id>/dislike/",
-        CommentsDislikeView.as_view(),
-        name="comment_dislike",
-    ),
     path('', views.FeedListView.as_view(), name="feedlist"),
     path('create_feed/', views.FeedCreateView.as_view(), name="feed_detail"),
     path('<int:author_id>/<int:feed_id>/', views.FeedDetailView.as_view(), name="feed_detail"),
--- feeds/views.py
+++ feeds/views.py
@@ -1,15 +1,6 @@
 from rest_framework.views import APIView
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-from feeds.models import Feed
-from feeds.serializers import FeedListSerializer, FeedCreateSerializer, FeedDetailSerializer
-=======
->>>>>>> f9d44eb (회원가입, 로그인, 수정 완료)
-=======
 from feeds.models import Feed, Comment
 from feeds.serializers import FeedListSerializer, FeedCreateSerializer, FeedDetailSerializer, CommentSerializer
->>>>>>> refs/remotes/origin/main
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
@@ -31,29 +22,6 @@ class FeedListView(APIView, ListView):
 
 class CommentsView(APIView):
     def get(self, request):
-<<<<<<< HEAD
-        #댓글 가져오기
-        return Response({"message": "comment get 요청입니다!"})
-=======
-from rest_framework.response import Response
-from .models import Feed, Comment
-from .serializers import FeedSerializer, CommentSerializer
-
-
-class FeedListView(APIView):
-    def get(self, request):
-        feeds = Feed.objects.all().order_by("-created_at")
-        serializer = FeedSerializer(feeds, many=True)
-
-        return Response(serializer.data)
->>>>>>> 0e77156 (댓글 생성,수정,삭제 / 피드 생성,수정,삭제)
-
-
-<<<<<<< HEAD
-    def update(self, request, comment_id):
-        #댓글 수정
-        return Response({"message": "comment update 요청입니다!"})
-=======
         # 댓글 가져오기
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
@@ -81,7 +49,6 @@ class FeedListView(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=400)
->>>>>>> refs/remotes/origin/main
 
     def delete(self, request, comment_id):
         # 댓글 삭제
@@ -123,27 +90,11 @@ class CommentsDislikeView(APIView):
 
 class FeedDetailView(APIView, HitCountDetailView):
     #feed 상세페이지
-=======
-class FeedDetailView(APIView):
-    def get(self, request, post_id):
-        try:
-            feed = Feed.objects.get(id=post_id)
-        except Feed.DoesNotExist:
-            return Response({"error": "피드가 없습니다."}, status=404)
-
-        serializer = FeedSerializer(feed)
-        return Response(serializer.data)
->>>>>>> 0e77156 (댓글 생성,수정,삭제 / 피드 생성,수정,삭제)
 
     # 조회수
     model = Feed    
     count_hit = True 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
     
->>>>>>> refs/remotes/origin/main
     # 탬플릿에서 조회수 나타내기
     # {# the total hits for the object #}
     # {{ hitcount.total_hits }}
@@ -152,6 +103,7 @@ class FeedDetailView(APIView):
         feed = get_object_or_404(Feed, id=feed_id)
         serializer = FeedDetailSerializer(feed)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class FeedCreateView(APIView):
     # feed 만들기 기능. 
@@ -165,7 +117,6 @@ class FeedCreateView(APIView):
 
 
 
-
 class FeedLikeView(APIView):
     def post(self, request, feed_id):
         feed = get_object_or_404(Feed, id=feed_id)
@@ -175,103 +126,3 @@ class FeedLikeView(APIView):
         else:
             feed.likes.add(request.user) #like 요청 유저가 없으면 추가
             return Response("좋아요 취소!", status=status.HTTP_200_OK)
-=======
-class FeedCreateView(APIView):
-    def post(self, request):
-        # 게시글 작성
-        serializer = FeedSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-    def put(self, request, post_id):
-        # 게시글 수정
-        try:
-            feed = Feed.objects.get(id=post_id)
-        except Feed.DoesNotExist:
-            return Response({"error": "피드가 없습니다."}, status=404)
-
-        serializer = FeedSerializer(feed, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-
-    def delete(self, request, post_id):
-        # 게시글 삭제
-        try:
-            feed = Feed.objects.get(id=post_id)
-        except Feed.DoesNotExist:
-            return Response({"error": "피드가 없습니다."}, status=404)
-
-        feed.delete()
-        return Response(status=204)
-
-
-class CommentsView(APIView):
-    def get(self, request):
-        # 댓글 가져오기
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        # 댓글 생성
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-    def put(self, request, comment_id):
-        # 댓글 수정
-        try:
-            comment = Comment.objects.get(id=comment_id)
-        except Comment.DoesNotExist:
-            return Response({"error": "댓글이 없습니다."}, status=404)
-
-        serializer = CommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-
-    def delete(self, request, comment_id):
-        # 댓글 삭제
-        try:
-            comment = Comment.objects.get(id=comment_id)
-        except Comment.DoesNotExist:
-            return Response({"error": "댓글이 없습니다."}, status=404)
-        comment.delete()
-
-        return Response({"message": "삭제되었습니다."}, status=204)
-
-
-class CommentsLikeView(APIView):
-    # 좋아요
-    def post(self, request, comment_id):
-        try:
-            comment = Comment.objects.get(id=comment_id)
-        except Comment.DoesNotExist:
-            return Response({"error": "댓글이 없습니다."}, status=404)
-
-        comment.like_count += 1
-        comment.save()
-
-        return Response(status=204)
-
-
-class CommentsDislikeView(APIView):
-    # 싫어요
-    def post(self, request, comment_id):
-        try:
-            comment = Comment.objects.get(id=comment_id)
-        except Comment.DoesNotExist:
-            return Response({"error": "댓글이 없습니다."}, status=404)
-
-        comment.dislike_count += 1
-        comment.save()
-
-        return Response(status=204)
->>>>>>> 0e77156 (댓글 생성,수정,삭제 / 피드 생성,수정,삭제)
