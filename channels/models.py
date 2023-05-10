fbac13ad82104b97b821eb05640189ba22ec7005
jisu <onpaso53@gmail.com>
2023-05-10 15:04:29 +0900

    fixbugs

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
 # Create your models here.
+# 채널 설명, 채널 이미지 ??
--- channels/views.py
+++ channels/views.py
@@ -37,19 +37,19 @@ class ChannelAdminView(APIView, HitCountDetailView):
         
         return context
 
-    def get(self, request, feed_id):
+    def get(self, request, user_id, feed_id):
         #개인 채널 관리 뷰
         #업로드한 동영상 또는 게시글 확인할 수 있고,
         #채널에 대한 총 구독자 수, 내가 팔로우한 사람, 좋아요 수와 조회수를 확인할 수 있다.
         permission_classes = [permissions.IsAuthenticated]
-        feeds = get_object_or_404(Feed, id= feed_id)
-        if request.user == feeds.user:
+        feeds = Feed.objects.filter(id = feed_id)
+        if request.user.id == user_id:
             serializer = FeedListSerializer(feeds, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
 
-    def put(self, request, feed_id):
+    def put(self, request, user_id, feed_id):
         # 관리페이지 안에서 수정
         permission_classes = [permissions.IsAuthenticated]
         feed = get_object_or_404(Feed, id=feed_id)
@@ -63,7 +63,7 @@ class ChannelAdminView(APIView, HitCountDetailView):
         else:
             return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self, request, feed_id):
+    def delete(self, request, user_id, feed_id):
         # 관리페이지 안에서 삭제
         permission_classes = [permissions.IsAuthenticated]
         feed = get_object_or_404(Feed, id=feed_id)
