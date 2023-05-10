f1e33cc5731ff3c745cbf5a854128aa2e4440af6
chanhyoYun <chloeee2@naver.com>
2023-05-10 09:11:55 +0900

    회원가입, 로그인 추가

--- channels/urls.py
+++ channels/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from channels import views
 
 urlpatterns = [
-    path('/<int:user_id>/', views.ChannelsView(), name="channel_view"),
-    path('/admin/<int:feed_id>/', views.ChannelAdminView(), name="channel_admin_view"),
+
 ]
--- feeds/admin.py
+++ feeds/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from feeds.models import Feed
-
-admin.site.register(Feed)
+# Register your models here.
