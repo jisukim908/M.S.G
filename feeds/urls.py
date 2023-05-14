09da003f178d67a53a87d680bcf4d39b3f1ec7aa
jisu <onpaso53@gmail.com>
2023-05-15 00:07:09 +0900

    코드정리

--- feeds/urls.py
+++ feeds/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('create_feed/', views.FeedCreateView.as_view(), name="feed_detail"),
     path('<int:feed_id>/', views.FeedDetailView.as_view(), name="feed_detail"),
     path('<int:feed_id>/likes/', views.FeedLikeView.as_view(), name="likes_feed"),
-    ### get에는 author_id가 없음.. views.py에 추가함!
     
     # path("comments/", views.CommentsView.as_view(), name="comments"),
     # path("comments/<int:comment_id>/", views.CommentsView.as_view(), name="comment_detail"),
